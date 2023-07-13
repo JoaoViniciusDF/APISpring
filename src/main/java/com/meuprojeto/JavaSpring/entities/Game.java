57e632f9f843e61ea0e439030899a0401cfbec51
João Vinicius <joaodessimonifernandes@outlook.com>
2023-07-12 22:21:34 -0300

    Game seed

--- src/main/java/com/meuprojeto/JavaSpring/entities/Game.java
+++ src/main/java/com/meuprojeto/JavaSpring/entities/Game.java
@@ -21,9 +21,14 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String platform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
-	private String shortDescriprion;
+	
+	@Column(columnDefinition = "TEXT")
+	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	
@@ -31,16 +36,17 @@ public class Game {
 	}
 
 
-	public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
-			String shortDescriprion, String longDescription) {
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
+			String shortDescription, String longDescription) {
 	
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
-		this.shortDescriprion = shortDescriprion;
+		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
 		
 	}
@@ -87,12 +93,22 @@ public class Game {
 
 
 	public String getPlatform() {
-		return platform;
+		return platforms;
 	}
 
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setPlatform(String platforms) {
+		this.platforms = platforms;
+	}
+
+
+	public Double getScore() {
+		return score;
+	}
+
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 
@@ -107,12 +123,12 @@ public class Game {
 
 
 	public String getShortDescriprion() {
-		return shortDescriprion;
+		return shortDescription;
 	}
 
 
-	public void setShortDescriprion(String shortDescriprion) {
-		this.shortDescriprion = shortDescriprion;
+	public void setShortDescriprion(String shortDescription) {
+		this.shortDescription = shortDescription;
 	}
 
 
