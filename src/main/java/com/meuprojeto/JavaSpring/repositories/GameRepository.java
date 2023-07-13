80d6ce721601333e8152debdd3563a4743354fb8
João Vinicius <joaodessimonifernandes@outlook.com>
2023-07-13 16:15:31 -0300

    GameRepository, GameService, GameController

---
+++ src/main/java/com/meuprojeto/JavaSpring/repositories/GameRepository.java
@@ -0,0 +1,11 @@
+package com.meuprojeto.JavaSpring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.meuprojeto.JavaSpring.entities.Game;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+	
+	
+
+}
---
+++ src/main/java/com/meuprojeto/JavaSpring/services/GameService.java
@@ -0,0 +1,27 @@
+package com.meuprojeto.JavaSpring.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.meuprojeto.JavaSpring.DTO.GameMinDTO;
+import com.meuprojeto.JavaSpring.entities.Game;
+import com.meuprojeto.JavaSpring.repositories.GameRepository;
+
+
+@Service
+public class GameService {
+
+	@Autowired
+	private GameRepository gameRepository;
+	
+	public List<GameMinDTO> findAll(){
+		
+		List<Game> result = gameRepository.findAll();
+		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+		return dto;
+		
+	}
+	
+}
