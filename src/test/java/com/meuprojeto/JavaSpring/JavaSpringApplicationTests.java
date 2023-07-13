e247d37782cad1a93118e7846290cdd194f44c90
João Vinicius <joaodessimonifernandes@outlook.com>
2023-07-12 21:27:30 -0300

    Project created

---
+++ src/test/java/com/meuprojeto/JavaSpring/JavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meuprojeto.JavaSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
