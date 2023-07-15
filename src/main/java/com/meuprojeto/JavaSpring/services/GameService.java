91c51b696bf631c3e2aabfdb55bb812bfa643f13
João Vinicius <joaodessimonifernandes@outlook.com>
2023-07-15 19:13:36 -0300

    GET game by id

--- src/main/java/com/meuprojeto/JavaSpring/services/GameService.java
+++ src/main/java/com/meuprojeto/JavaSpring/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.meuprojeto.JavaSpring.DTO.GameDTO;
 import com.meuprojeto.JavaSpring.DTO.GameMinDTO;
 import com.meuprojeto.JavaSpring.entities.Game;
 import com.meuprojeto.JavaSpring.repositories.GameRepository;
@@ -16,7 +18,16 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		
+		Game result = gameRepository.findById(id).get(); 
+		GameDTO dto = new GameDTO(result);
+		return dto;
+		
+	}
 	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		
 		List<Game> result = gameRepository.findAll();
